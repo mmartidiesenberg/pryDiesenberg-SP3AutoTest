e96078a066a35ec8d74a760f8f5dc937d98bc82f
mmartidiesenberg <mmmartidiesenberg@gmail.com>
2025-10-17 12:13:38 -0300

    .

--- pryDiesenberg-SP3AutoTest/frmAutotest.Designer.cs
+++ pryDiesenberg-SP3AutoTest/frmAutotest.Designer.cs
@@ -243,7 +243,7 @@
             this.btnRegistrar.TabIndex = 2;
             this.btnRegistrar.Text = "Registrar";
             this.btnRegistrar.UseVisualStyleBackColor = true;
-            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+
             // 
             // txtDominio
             // 
